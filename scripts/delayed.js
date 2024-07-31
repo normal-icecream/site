754facb3ebdf3385795dd647f3bf5e8be717f81d
fkakatie <ksexton@adobe.com>
2024-07-30 22:40:02 -0600

    feat: external link handling

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,7 @@
 // add delayed functionality here
+function decorateExternalLinks(links) {
+  links.forEach((link) => link.setAttribute('target', '_blank'));
+}
+
+const externalLinks = document.querySelectorAll('a[href]');
+decorateExternalLinks(externalLinks);
