c8cd1885bd9339d00f57e18293d238ac05cac9dc
fkakatie <ksexton@adobe.com>
2024-08-27 12:26:40 -0600

    refactor: new variants, new comments, slightly new dom structure to accomodate

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,68 +1,247 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const contentWrapper = document.createElement('div');
-    contentWrapper.className = 'cards-card-content';
-    const li = document.createElement('li');
+/**
+ * Delays execution of a function until delay has passed since the last function invocation.
+ * @param {Function} func - Function to debounce
+ * @param {number} delay - Delay (in milliseconds) to wait before invoking the function
+ * @returns {Function} Debounced version of the input function
+ */
+function debounce(func, delay) {
+  let timeout;
+  // eslint-disable-next-line func-names
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), delay);
+  };
+}
 
-    // running a test
+/**
+ * Decreases the value of an input element by 1 (while observing the min value).
+ * @param {HTMLInputElement} input - Input element whose value will be decremented.
+ */
+function decrement(input) {
+  const total = parseInt(input.value, 10);
+  const min = parseInt(total.min, 10) || 0;
+  if (total > min) {
+    input.value = total - 1;
+    input.dispatchEvent(new Event('change'));
+    // TODO: update cart totals
+  }
+}
 
-    while (row.firstElementChild) contentWrapper.append(row.firstElementChild);
+/**
+ * Increases the value of an input element by 1 (while observing the max value).
+ * @param {HTMLInputElement} input - Input element whose value will be incremented.
+ */
+function increment(input) {
+  const total = parseInt(input.value, 10);
+  const max = parseInt(total.max, 10) || null;
+  if (!max || total < max) {
+    input.value = total + 1;
+    input.dispatchEvent(new Event('change'));
+    // TODO: update cart totals
+  }
+}
 
-    [...contentWrapper.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
+/**
+ * Checks if an element has a vertical scrollbar.
+ * @param {HTMLElement} el - Element to check.
+ * @returns {boolean} True if the element has a vertical scrollbar, false if not.
+ */
+function hasScrollbar(el) {
+  return el.scrollHeight > el.clientHeight;
+}
 
-    li.append(contentWrapper);
+/**
+ * Gets the computed style value of a specified property on an element.
+ * @param {HTMLElement} el - Element whose style property value is being retrieved.
+ * @param {string} prop - Name of the style property to retrieve.
+ * @returns {number} Numerical value of the specified style property.
+ */
+function getStyle(el, prop) {
+  const styles = window.getComputedStyle(el);
+  const value = styles.getPropertyValue(prop);
+  // convert the style property value to a number, stripping 'px' unit
+  return parseFloat(value.replace('px', ''), 10);
+}
 
-    // decorate card cart actions
-    const cartUpdateWrapper = document.createElement('div');
-    cartUpdateWrapper.className = 'cards-card-cart-update';
+/**
+ * Removes the inline styles used for clamping text.
+ * @param {HTMLElement} wrapper - Parent element wrapping paragraphs to unclamp.
+ */
+function unclampBodies(wrapper) {
+  const clamped = wrapper.querySelectorAll('p[style]');
+  clamped.forEach((el) => el.removeAttribute('style'));
+}
 
-    const decrementWrapper = document.createElement('div');
-    decrementWrapper.className = 'cards-card-update-button';
-    const decrementButton = document.createElement('button');
-    decrementButton.setAttribute('type', 'button');
-    decrementButton.innerHTML = '-';
-    // decrementButton.addEventListener('click', () => {console.log('decrement clicked')});
-    decrementWrapper.append(decrementButton);
-    cartUpdateWrapper.append(decrementWrapper);
-
-    const cartItemCount = document.createElement('div');
-    cartItemCount.setAttribute('type', 'text');
-    cartItemCount.className = 'cards-card-cart-item-count';
-    cartItemCount.innerHTML = '0'; // TODO - dynamically add item count
-    cartUpdateWrapper.append(cartItemCount);
-
-    const incrementWrapper = document.createElement('div');
-    incrementWrapper.className = 'cards-card-update-button';
-    const incrementButton = document.createElement('button');
-    incrementButton.setAttribute('type', 'button');
-    incrementButton.innerHTML = '+';
-    // incrementButton.addEventListener('click', () => console.log('increment clicked'));
-    incrementWrapper.append(incrementButton);
-    cartUpdateWrapper.append(incrementWrapper);
-
-    li.append(cartUpdateWrapper);
-    ul.append(li);
+/**
+ * Clamps the description text inside card body elements based on available space.
+ * @param {HTMLElement} wrapper - Parent element wrapping card body elements.
+ */
+function clampBodies(wrapper) {
+  const bodies = wrapper.querySelectorAll('.cards-card-body');
+  bodies.forEach((body) => {
+    const overflows = hasScrollbar(body);
+    if (overflows) {
+      const head = body.querySelector('h3');
+      const desc = body.querySelector('h3 ~ p:not([class])');
+      if (desc) {
+        const headHeight = head.offsetHeight;
+        const lineHeight = getStyle(desc, 'line-height');
+        const spacing = getStyle(desc, 'margin-top');
+        const heightAvail = body.clientHeight - headHeight - spacing;
+        let linesAvail = Math.floor(heightAvail / lineHeight);
+        if (linesAvail < 0) linesAvail = 0;
+        desc.style.setProperty('--line-clamp', linesAvail);
+      }
+    } else unclampBodies(body); // unclamp the body if it doesn't overflow
   });
-  const lists = document.querySelectorAll('.cards ul');
-  if (lists.length > 0) {
-    lists.forEach((list) => {
-      list.querySelectorAll('img').forEach((img) => {
-        const picture = img.closest('picture');
+}
 
-        if (picture) {
-          img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
+export default function decorate(block) {
+  const variants = [...block.classList];
+  // reorganize cards in ordered list
+  const ul = document.createElement('ul');
+
+  // decorate each card
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    const card = document.createElement('div');
+    card.className = 'cards-card';
+    const [image, body] = row.children;
+    image.className = 'cards-card-image';
+    body.className = 'cards-card-body';
+    card.append(image, body);
+
+    // decorate image
+    const img = image.querySelector('picture > img');
+    if (img) {
+      const pic = img.closest('picture');
+      pic.replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '560' }]));
+    }
+
+    // decorate dietary restrictions
+    const dietary = body.querySelector('h3 ~ p > span.icon');
+    if (dietary) {
+      const p = dietary.closest('p');
+      if (p.textContent === dietary.textContent) p.className = 'cards-card-dietary';
+    }
+
+    // decorate badge
+    const badge = body.querySelector('h3 ~ p > em');
+    if (badge) {
+      const p = badge.closest('p');
+      if (p.textContent === badge.textContent) {
+        p.className = 'cards-card-badge';
+        p.innerHTML = badge.innerHTML;
+      }
+    }
+
+    // decorate price
+    if (variants.includes('price')) {
+      const price = body.querySelector('h3 ~ p > strong');
+      if (price) {
+        const p = price.closest('p');
+        if (p.textContent === price.textContent) {
+          p.className = 'cards-card-price';
+          p.innerHTML = price.innerHTML;
+          // style flat rate
+          if (p.textContent.includes('flat rate')) {
+            p.textContent = p.textContent.replace('flat rate', '').trim();
+            const span = document.createElement('span');
+            span.className = 'flat-rate';
+            span.textContent = 'flat rate';
+            p.append(span);
+          }
+        }
+      }
+    }
+
+    // assemble card
+    li.append(card);
+
+    if (!variants.includes('customize')) {
+      // decorate cart actions
+      const cart = document.createElement('form');
+      cart.className = 'cards-card-cart';
+
+      // build total field
+      const total = document.createElement('input');
+      total.type = 'number';
+      total.min = 0;
+      total.value = 0; // TODO: pull from cart totals
+      total.step = 1;
+      total.readOnly = true;
+      total.addEventListener('change', () => {
+        const value = parseInt(total.value, 10);
+        const subtract = cart.querySelector('.button.subtract');
+        const min = parseInt(total.min, 10) || 0;
+        if (value > min) subtract.removeAttribute('disabled');
+        else subtract.disabled = true;
+        // TODO: disable "add" if max
+      });
+      cart.append(total);
+
+      // build action buttons
+      const actions = ['subtract', 'add'];
+      actions.forEach((action) => {
+        const button = document.createElement('button');
+        button.className = `button ${action}`;
+        button.type = 'button';
+        // TODO: write more dynamic button labels
+        button.setAttribute('aria-label', `${action} item`);
+        const symbol = document.createElement('i');
+        symbol.className = `symbol symbol-${action}`;
+        button.append(symbol);
+        if (action === 'subtract') {
+          button.disabled = true;
+          cart.prepend(button);
+          button.addEventListener('click', () => decrement(total));
+        } else {
+          cart.append(button);
+          button.addEventListener('click', () => increment(total));
         }
       });
-    });
-  }
+      li.append(cart);
+    } else {
+      // build customize button
+      const button = document.createElement('button');
+      button.className = 'button customize';
+      button.type = 'button';
+      // TODO: write more dynamic button label
+      button.setAttribute('aria-label', 'customize item');
+      button.textContent = 'customize';
+      button.addEventListener('click', () => {
+        // TODO: open customize menu
+      });
+      li.append(button);
+    }
 
-  block.textContent = '';
+    // assemble card
+    ul.append(li);
+  });
+
+  block.innerHTML = '';
   block.append(ul);
+
+  // observe the block attributes to determine when it appears in the dom
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(() => {
+      // clamp bodies when their height can be calculated
+      setTimeout(() => clampBodies(ul), 100);
+      observer.disconnect();
+    });
+  });
+  observer.observe(block, { attributes: true });
+
+  // listen for resize events to determine when elements should or shouldn't be clamped
+  window.addEventListener('resize', debounce((e) => {
+    const width = e.target.innerWidth;
+    // apply clamping on devices larger than mobile
+    if (width >= 600) {
+      unclampBodies(ul);
+      clampBodies(ul);
+    } else unclampBodies(ul); // unclamp all bodies on mobile
+  }, 100));
 }
