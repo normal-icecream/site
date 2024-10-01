5a8cbb53f1e3e5f0aae1a8fc13a8c95de1560f2a
Andi Carlstrom <andi.carlstrom@gmail.com>
2024-10-01 14:09:56 -0600

    Fixed linting errors.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,15 +1,12 @@
 // Function to remove empty elements based on selector
-function removeEmptyElements(selector) {
+export function removeEmptyElements(selector) {
   const elements = document.querySelectorAll(selector);
 
   elements.forEach((element) => {
     // Check if the element has child nodes
     if (element.children.length === 0) {
-    element.remove();
+      element.remove();
     }
   });
 }
 
-export {
-    removeEmptyElements,
-};
