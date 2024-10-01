61b7a3c3b198b21ed29783f69e693c28f22895c8
Andi Carlstrom <andi.carlstrom@gmail.com>
2024-10-01 13:39:52 -0600

    Removed h3 from columns for symantic reasons, edited table.js table build to create only one table element and then add all theads and tbodys one after another in the dom. Lastly, added helper to remove html elements that dont have children.

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,11 +1,12 @@
+import { removeEmptyElements } from "../../helpers/helpers.js";
+
 function buildCell(rowIndex) {
   const cell = rowIndex ? document.createElement('td') : document.createElement('th');
   if (!rowIndex) cell.setAttribute('scope', 'col');
   return cell;
 }
 
-export default async function decorate(block) {
-  const table = document.createElement('table');
+function buildTable(block, table) {
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
 
@@ -27,4 +28,24 @@ export default async function decorate(block) {
   });
   block.innerHTML = '';
   block.append(table);
+}
+
+export default async function decorate(block) {
+  const tableElement = document.querySelectorAll('table');
+
+  // Create a new table element and add block table content to it.
+  if (tableElement.length === 0) {
+    const table = document.createElement('table');
+    buildTable(block, table);
+  }
+
+  // If a table exists in the dom, add to it instead of creating a
+  // new one and then add block table content to it.
+  if (tableElement.length === 1) {
+    buildTable(block, tableElement[0]);
+  }
+
+  // Remove empty '.table.block' and '.table-wrapper' elements
+  removeEmptyElements('.table-container .table.block');
+  removeEmptyElements('.table-wrapper');
 }
