047c2e668c0327424d19161d8cd0b48676749226
Andi Carlstrom <andi.carlstrom@gmail.com>
2024-10-03 13:39:44 -0600

    Replaced logic that was displaying the pricetag svg as a background image and instead am rendering it as an svg element.

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -1,39 +1,52 @@
+// import { decorateIcons } from "../../scripts/aem";
+
 export default function decorate(block) {
   const variants = [...block.classList];
   [...block.children].forEach((row) => {
     // TODO - need to add counter to title, should be set up when doing Square integration
-
+    
     // decorate price tag
     if (variants.includes('price')) {
       const price = row.querySelector('p > strong');
       if (price) {
-        const p = price.closest('p');
-        if (p.textContent === price.textContent) {
-          p.className = 'title-price';
+        const svgPath = '../../icons/pricetag.svg';
 
-          // separate out price tag amount & subtext
-          const dollarAmount = price.textContent.match(/\$\d+/)[0];
-          const subtext = price.textContent.replace(dollarAmount, '').trim();
+        const divContainer = row.querySelector('.title.price > div > div');
 
-          // containing div to prevent content from rotating with the animated price tag
-          const contentDiv = document.createElement('div');
-          contentDiv.className = 'title-price-content';
+        const p = divContainer.querySelector('p:has(strong)');
+        p.className = 'title-price';
 
-          const amountDiv = document.createElement('div');
-          amountDiv.textContent = dollarAmount;
-          amountDiv.className = 'title-price-amount';
+        // separate out price tag amount & subtext
+        const dollarAmount = price.textContent.match(/\$\d+/)[0];
+        const subtext = price.textContent.replace(dollarAmount, '').trim();
 
-          const textContentLength = price.textContent.length;
-          const subtextDiv = document.createElement('div');
-          subtextDiv.textContent = subtext;
-          subtextDiv.className = textContentLength > 10 ? 'title-price-subtext title-price-long-subtext' : 'title-price-subtext';
+        // containing div to prevent content from rotating with the animated price tag
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'title-price-content';
 
-          contentDiv.append(amountDiv, subtextDiv);
+        const amountDiv = document.createElement('div');
+        amountDiv.textContent = dollarAmount;
+        amountDiv.className = 'title-price-amount';
 
-          // replace original strong element with newly formatted elements
-          const strongElement = p.querySelector('strong');
-          strongElement.replaceWith(contentDiv);
-        }
+        const textContentLength = price.textContent.length;
+        const subtextDiv = document.createElement('div');
+        subtextDiv.textContent = subtext;
+        subtextDiv.className = textContentLength > 10 ? 'title-price-subtext title-price-long-subtext' : 'title-price-subtext';
+
+        contentDiv.append(amountDiv, subtextDiv);
+        
+        const strongElement = p.querySelector('strong');
+        strongElement.replaceWith(contentDiv);
+
+        const span = document.createElement('span');
+
+        fetch(svgPath)
+          .then(response => response.text())
+          .then(svgContent => {
+            span.innerHTML = svgContent;
+            p.append(span)
+          })
+          .catch(error => console.log('error loading svg from relative path:', error))
       }
     }
   });
