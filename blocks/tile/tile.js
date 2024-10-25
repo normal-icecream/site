8a525df7392ee735d88118fef0848e08fc6189ad
Andi Carlstrom <andi.carlstrom@gmail.com>
2024-10-25 12:17:51 -0600

    Linting fixes.

--- blocks/tile/tile.js
+++ blocks/tile/tile.js
@@ -1,14 +1,12 @@
 export default function decorate(block) {
-    const picture = block.querySelector('.tile > div > div > picture');
-    const card = block.querySelector('.tile > div > div:last-child');
-    const icon = block.querySelector('.tile > div > div:last-child p.img-wrapper');
+  const picture = block.querySelector('.tile > div > div > picture');
+  const card = block.querySelector('.tile > div > div:last-child');
+  const icon = block.querySelector('.tile > div > div:last-child p.img-wrapper');
+  const cardContainer = document.createElement('div');
+  const cardContent = document.createElement('div');
 
-    const cardContainer = document.createElement('div');
-    const cardContent = document.createElement('div');
-    cardContent.append(card, icon);
-    cardContainer.append(cardContent)
-
-    block.innerHTML = '';
-
-    block.append(picture, cardContainer);
+  cardContent.append(card, icon);
+  cardContainer.append(cardContent);
+  block.innerHTML = '';
+  block.append(picture, cardContainer);
 }
