02ace2f2782b7877c5e84745705394d81c0bde46
fkakatie <ksexton@adobe.com>
2025-01-05 11:50:01 -0700

    chore: fix overeager decorateExternalLinks

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,6 +1,12 @@
 // add delayed functionality here
 function decorateExternalLinks(links) {
-  links.forEach((link) => link.setAttribute('target', '_self'));
+  const { origin } = new URL(window.document.location);
+  links.forEach((link) => {
+    const { origin: linkOrigin } = new URL(link.href);
+    if (linkOrigin && origin !== linkOrigin) {
+      link.setAttribute('target', '_self');
+    }
+  });
 }
 
 const externalLinks = document.querySelectorAll('a[href]');
