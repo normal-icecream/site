909320d79264d0d014abb2dc2a24ef1e6d3944db
Andi Carlstrom <andi.carlstrom@gmail.com>
2025-01-21 16:32:12 -0700

    Cleared out commented out cart code.

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -22,85 +22,6 @@ function getEmptyCartMessage() {
     return noCartDiv;
 }
 
-// function getCartCard(cartItems) {
-//     // Fetch catalog from Square
-//     const cartCardWrapper = document.createElement('div');
-//     cartCardWrapper.className = 'cart card-wrapper';
-
-//     cartItems.forEach(item => {
-//         const cartCard = document.createElement('div');
-//         cartCard.className = 'cart card';
-//         cartCard.textContent = item.title;
-//         cartCardWrapper.append(cartCard);
-
-//         const buttonWrapper = document.createElement('div');
-//         buttonWrapper.className = 'cart card-button-wrapper';
-        
-//         const decrement = document.createElement('button');
-//         decrement.className = 'cart card-decrement';
-//         decrement.textContent = '-';
-//         decrement.addEventListener('click', () => removeFromCart(item));
-//         buttonWrapper.append(decrement);
-        
-//         const increment = document.createElement('button');
-//         increment.className = 'cart card-increment';
-//         increment.textContent = '+';
-//         increment.addEventListener('click', () => addToCart(item));
-//         buttonWrapper.append(increment);
-
-//         cartCardWrapper.append(buttonWrapper);
-//     })
-
-//     return cartCardWrapper;
-// }
-
-// export function getSandboxId(prodId) {
-//     const prodToSandboxMap = JSON.parse(localStorage.getItem('prodToSandboxMap')) || [];
-//     const item = prodToSandboxMap.find((item) => item.prodId === prodId);
-
-//     if (item) {
-//         return item.sandboxId;
-//     } else {
-//         return null;
-//     }
-// }
-
-// function setSandboxId(prodId, sandboxId) {
-//     const prodToSandboxMap = JSON.parse(localStorage.getItem('prodToSandboxMap')) || [];
-    
-//     const match = prodToSandboxMap.find(item => item.prodId === prodId);
-//     if (!match) {
-//       prodToSandboxMap.push({ prodId, sandboxId });
-//     }
-//     localStorage.setItem('prodToSandboxMap', JSON.stringify(prodToSandboxMap))
-// }
-
-// export async function getSandboxOrProdItem(squareProdItemId) {
-//     const env = getEnvironment();
-//     let cardItem = {};
-
-//     // Need to make sure thism request ALWAYS hits prod
-//     const prodItem = await hitProduction(getCatalogItem, squareProdItemId);
-        
-//     if (env === 'sandbox') {
-//         const sandboxCatalogItems = await hitSandbox(getCatalogList);
-//         const filteredSandboxItems = sandboxCatalogItems?.filter((item) => item.type === 'ITEM');
-//         const sandboxDupeOfProdItem = filteredSandboxItems?.find((item) => item.item_data.name === prodItem.item_data.name);
-    
-//         if (!sandboxDupeOfProdItem) {
-//             const sandboxItem = await upsertCatalogItem(prodItem);
-//             cardItem = sandboxItem;
-//             // setSandboxId(squareProdItemId, sandboxItem.id);
-//         } else {
-//             cardItem = sandboxDupeOfProdItem;
-//             // setSandboxId(squareProdItemId, sandboxDupeOfProdItem.id);
-//         }
-//     } else {
-//         cardItem = prodItem
-//     }
-//     return cardItem;
-// }
-
 export async function addItemToCart(id) {
     const carts = JSON.parse(localStorage.getItem('carts'));
     const cartKey = getLastCart();
@@ -137,40 +58,6 @@ export function removeItemFromCart(id) {
     }
     localStorage.setItem(`carts`, JSON.stringify(carts));
 }
-
-// export async function upsertItemToCart(squareProdItemId) {
-//     const env = getEnvironment();
-//     const carts = JSON.parse(localStorage.getItem('carts'));
-//     const cartKey = getLastCart();
-//     const cart = carts[cartKey]?.line_items || [];
-//     const cartId = env === 'sandbox' ? getSandboxId(squareProdItemId) : squareProdItemId;
-//     const itemIsAlreadyInCart = cart.some((item) => env === 'sandbox' 
-//         ? item.id === getSandboxId(squareProdItemId) 
-//         : item.id === squareProdItemId);
-
-//     const quantity = 1; // Default quantity for a new item
-//     if (itemIsAlreadyInCart) {
-//         const cartItem = cart.find((item) => item.id === cartId);
-//         cartItem.quantity += quantity;
-//     } else {
-//         // TODO is there better logic here???
-//         // TODO - I think that get card item may not need to return the entire item. Maybe just the ID and quantity????
-//         let squareItem = await getSandboxOrProdItem(squareProdItemId);
-//         const cartItem = cart.find((item) => item.id === squareItem.id);
-
-//     if (cartItem) {
-//         // increment
-//         cartItem.quantity += quantity;
-//     } else {
-//         // add item to cart
-//         cart?.push({
-//             id: squareItem.id,
-//             quantity: quantity,
-//         });
-//     }
-//   }
-//   localStorage.setItem('carts', JSON.stringify(carts));
-// }
 
 export function getCartItemQuantity(prodId) {
     const cart = getLocalStorageCart();
