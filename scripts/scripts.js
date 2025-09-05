bae0420ee528dd444b362d1eda0fac68532dc62b
Andi Carlstrom <andi.carlstrom@gmail.com>
2025-09-05 15:41:42 -0600

    Added indexedDB implementation to store Square catalog data.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-cycle */
+/* eslint-disable max-len */
 import {
   buildBlock,
   decorateBlock,
@@ -241,22 +242,12 @@ export async function fetchCatalog() {
   if (json) {
     const catalog = {
       byId: {},
-      items: [],
-      categories: [],
       discounts: {},
       taxList: [],
     };
     json.forEach((e) => {
       if (!catalog.byId[e.id]) {
         catalog.byId[e.id] = e;
-      }
-      if (e.type === 'ITEM') {
-        catalog.items.push(e);
-        if (e.item_data.variations) {
-          e.item_data.variations.forEach((v) => {
-            catalog.byId[v.id] = v;
-          });
-        }
       }
       if (e.type === 'MODIFIER_LIST') {
         if (e.modifier_list_data.modifiers) {
@@ -274,8 +265,39 @@ export async function fetchCatalog() {
       if (e.type === 'TAX') {
         catalog.taxList.push(e);
       }
-      if (e.type === 'CATEGORY') {
-        catalog.categories.push(e);
+    });
+    catalogData = catalog;
+  }
+  return catalogData;
+}
+
+export async function organizeCatalog(json) {
+  let catalogData;
+  if (json) {
+    const catalog = {
+      byId: {},
+      discounts: {},
+      taxList: [],
+    };
+    json.forEach((e) => {
+      if (!catalog.byId[e.id]) {
+        catalog.byId[e.id] = e;
+      }
+      if (e.type === 'MODIFIER_LIST') {
+        if (e.modifier_list_data.modifiers) {
+          e.modifier_list_data.modifiers.forEach((m) => {
+            m.modifier_data.modifier_list_id = e.id;
+            catalog.byId[m.id] = m;
+          });
+        }
+      }
+      if (e.type === 'DISCOUNT') {
+        if (e.discount_data.name) {
+          catalog.discounts[e.discount_data.name.toLowerCase()] = { id: e.id };
+        }
+      }
+      if (e.type === 'TAX') {
+        catalog.taxList.push(e);
       }
     });
     catalogData = catalog;
@@ -283,21 +305,216 @@ export async function fetchCatalog() {
   return catalogData;
 }
 
-export async function refreshCatalog() {
-  const catalogData = await fetchCatalog();
-  localStorage.setItem('catalog', JSON.stringify(catalogData));
+/**
+ * @function openSquareCatalogDb
+ * @description Opens (or creates, if it doesn't exist) the `normalStore` IndexedDB database
+ * and ensures a `catalog` object store exists. Provides a database connection for
+ * performing read/write transactions on the catalog.
+ * @returns {Promise<IDBDatabase>} A promise that resolves with the open IndexedDB database connection.
+ * @throws {DOMException} If the database fails to open or an IndexedDB error occurs.
+ */
+export function openSquareCatalogDb() {
+  return new Promise((resolve, reject) => {
+    // open a connection with indexedDB
+    const openRequest = indexedDB.open('normalStore', 1);
+
+    // Handle upgrade to db if necessary
+    openRequest.onupgradeneeded = (event) => {
+      const db = event.target.result;
+
+      // If catalog store DOESN'T exist then create catalog store
+      if (!db.objectStoreNames.contains('catalog')) {
+        db.createObjectStore('catalog', { keyPath: 'id' });
+      }
+    };
+
+    // Set onSuccess response handler if connection is successful
+    openRequest.onsuccess = (event) => {
+      resolve(event.target.result); // returns the db connection
+    };
+
+    // Set onError response handler if connection is not successful
+    openRequest.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
 }
 
-// Function to fetch the square catalog data on demand.
+/**
+ * Retrieves Square Catalog data from Square API and adds data to IndexedDB catalog store
+ * @async
+ * @function refreshCatalog
+ * @returns {Promise<Object[]>} A promise that resolves with an array of organized catalog items.
+ * @throws {DOMException} If an error occurs while reading from IndexedDB.
+* */
+async function refreshCatalog() {
+  // get database
+  const db = await openSquareCatalogDb();
+
+  // fetch square catalog data
+  const squareCatalog = await getCatalogListJson();
+
+  // open database for a transaction/modification
+  const transaction = db.transaction('catalog', 'readwrite');
+
+  // get store where an update is to be made
+  const store = transaction.objectStore('catalog');
+
+  // add data to store
+  squareCatalog.forEach((squareCatalogItem) => {
+    store.add(squareCatalogItem);
+  });
+
+  // return organized catalog array
+  return new Promise((resolve, reject) => {
+    const request = store.getAll();
+
+    request.onsuccess = async () => {
+      if (request.result !== undefined) {
+        // const catalogData = request.result;
+        const organizedCatalog = await organizeCatalog(request.result);
+        // catalog = organizedCatalog;
+        resolve(organizedCatalog);
+
+        // close db connection
+        db.close();
+      }
+    };
+
+    // Set onError response handler if fetching catalog data is not successful
+    request.onerror = (event) => {
+      reject(event.target.error);
+
+      // close db connection
+      db.close();
+    };
+  });
+}
+
+/**
+ * @async
+ * @function deleteAndReloadCatalog
+ * @description Deletes the entire IndexedDB database (`normalStore`) and reloads it with fresh catalog data.
+ * This ensures a clean slate by wiping all stored data, avoiding versioning issues, and reinitializing with the latest catalog.
+ * Also resets the catalog timestamp in `localStorage` to the current time.
+ * @returns {Promise<void>} A promise that resolves when the database has been deleted and the reload process has started.
+ * @throws {DOMException} If an error occurs while attempting to delete the database.
+ */
+async function deleteAndReloadCatalog() {
+  return new Promise((resolve, reject) => {
+    // Run delete database, we want to wipe all data and create new store to avoid indexedDB versioning.
+    const deleteReq = indexedDB.deleteDatabase('normalStore');
+    deleteReq.onsuccess = async () => {
+      // reset timestamp
+      const now = new Date().getTime();
+      localStorage.setItem('catalogTimestamp', JSON.stringify(now));
+
+      // I'm not awaiting this call, I want this to run in the background
+      await refreshCatalog();
+
+      resolve();
+    };
+
+    deleteReq.onerror = (e) => {
+      // eslint-disable-next-line no-console
+      console.error('Error deleting DB', e);
+      reject(e.target.error);
+    };
+
+    deleteReq.onblocked = () => {
+      // eslint-disable-next-line no-console
+      console.warn('Delete blocked — make sure all DB connections are closed!');
+    };
+  });
+}
+
+/**
+ * Retrieves and organizes the Square catalog data stored in IndexedDB.
+ * @async
+ * @function getCatalogFromIndexedDB
+ * @returns {Promise<Object[]>} A promise that resolves with an array of organized catalog items.
+ * @throws {DOMException} If an error occurs while reading from IndexedDB.
+* */
+async function getCatalogFromIndexedDB() {
+  const db = await openSquareCatalogDb();
+
+  // open database for a transaction/modification
+  const transaction = db.transaction('catalog', 'readwrite');
+
+  // get store where an update is to be made
+  const store = transaction.objectStore('catalog');
+
+  return new Promise((resolve, reject) => {
+    // fetch all catalog items from indexedDB store
+    const request = store.getAll();
+
+    request.onsuccess = async () => {
+      if (request.result !== undefined) {
+        // request.result is the entire raw list of Square catalog items that are stored in the catalog IndexedDB store
+        // reformat catalog data for the main app
+        const organizedCatalog = await organizeCatalog(request.result);
+
+        // return catalog data
+        resolve(organizedCatalog);
+
+        // close db connection
+        db.close();
+      }
+    };
+
+    // if an error occurs, return error message
+    request.onerror = (event) => {
+      reject(event.target.error);
+
+      // close db connection
+      db.close();
+    };
+  });
+}
+
+/**
+ * Fetches the square catalog data.
+ * @documentation https://javascript.info/indexeddb
+ * @returns {Promise<Array>} A promise that resolves to an array of catalog item objects.
+ * @throws {Error} - Throws an error if the catalog retreival fails.
+*/
+// rename getCatalog
 export async function getCatalog() {
-  const catalog = localStorage.getItem('catalog');
-  if (!catalog) {
-    const catalogData = await fetchCatalog();
-    localStorage.setItem('catalog', JSON.stringify(catalogData));
-    return catalogData;
+  const catalogTimestamp = JSON.parse(localStorage.getItem('catalogTimestamp'));
+  let catalog;
+
+  // If a catalog timestamp exists
+  if (catalogTimestamp) {
+    const now = new Date().getTime();
+    const twentyFourHoursFromTimeStamp = Number(catalogTimestamp) + (1 * 24 * 60 * 60 * 1000);
+
+    // If current time and date is less than 24 hours from timestamp
+    if (now < twentyFourHoursFromTimeStamp) {
+      catalog = await getCatalogFromIndexedDB();
+
+    // If current time is greater than or equal to 24 hours from timestamp
+    } else if (now >= twentyFourHoursFromTimeStamp) {
+      // grab stored catalog data for use before it is replaced by updated Square catalog data
+      catalog = await getCatalogFromIndexedDB();
+
+      // Delete catalog timestamp to be reset below
+      localStorage.removeItem('catalogTimestamp');
+
+      // delete catalog store, re-add it and then populate it with newly fetched square catalog data
+      await deleteAndReloadCatalog();
+    }
+
+  // brand new customer with no timestamp, create new catalog store in db, populate it with data and return catalog data
+  } else {
+    const now = new Date().getTime();
+    // set timestamp in local storage
+    localStorage.setItem('catalogTimestamp', JSON.stringify(now));
+
+    // Fetch Square catalog
+    catalog = await refreshCatalog();
   }
-  refreshCatalog();
-  return JSON.parse(catalog);
+
+  return catalog;
 }
 
 /**
